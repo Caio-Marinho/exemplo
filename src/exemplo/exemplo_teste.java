5d17e2849c3b9853304acdcd29e266516f913757
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-12 20:00:10 -0300

    comentario indicando operador ternario

--- src/exemplo/exemplo_teste.java
+++ src/exemplo/exemplo_teste.java
@@ -5,7 +5,8 @@ public class exemplo_teste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for (int i=0;i<=10;i++) {
-			System.out.println(i % 2 == 0 ? i +" é par" : i +" é impar");
+			// Operador ternario age como um if-else em situações que não exigem uma complexidade.
+			System.out.println(i % 2 == 0 ? i +" é par" : i +" é impar"); // Operador ternario {condição} ? {caso seja verdade} : {caso seja falso}
 		}
 
 	}
